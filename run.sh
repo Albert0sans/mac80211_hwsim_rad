7662aea0587b30e42e48ce30bd2cdc6b7af40360
Albert0sans <72274953+Albert0sans@users.noreply.github.com>
2022-09-22 22:20:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+sudo apt install flex bison
+
 make clean
-make
-sudo modprobe mac80211
+make 
+
 sudo modprobe mac80211_hwsim -r
+sudo modprobe mac80211
+#sudo ln -sf /usr/lib/modules/$(uname -a)/vmlinux.xz /boot/
+
 sudo insmod mac80211_hwsim.ko
