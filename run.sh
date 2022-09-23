c4908218b64cc97f6e95cae9ee17b385dc7d27b5
Albert0sans <72274953+Albert0sans@users.noreply.github.com>
2022-09-23 13:16:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@ sudo apt remove --purge linux-headers-* -y
 sudo apt autoremove && sudo apt autoclean -y
 sudo apt install linux-headers-generic -y
 make clean
-make 
+make
 
 sudo modprobe mac80211_hwsim -r
 sudo modprobe mac80211
 sudo ln -sf /usr/lib/modules/$(uname -a)/vmlinux.xz /boot/
 
-sudo insmod mac80211_hwsim.ko
+sudo insmod mac80211_hwsim.ko radios=$1
+
