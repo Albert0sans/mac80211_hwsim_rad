2d9eabacbe00c2da10b9311a5f173f0349b0816f
Albert0sans <72274953+Albert0sans@users.noreply.github.com>
2022-09-23 12:51:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-sudo apt install flex bison
-
+sudo apt update && sudo apt upgrade -y
+sudo apt remove --purge linux-headers-* -y
+sudo apt autoremove && sudo apt autoclean -y
+sudo apt install linux-headers-generic -y
 make clean
 make 
 
 sudo modprobe mac80211_hwsim -r
 sudo modprobe mac80211
-#sudo ln -sf /usr/lib/modules/$(uname -a)/vmlinux.xz /boot/
+sudo ln -sf /usr/lib/modules/$(uname -a)/vmlinux.xz /boot/
 
 sudo insmod mac80211_hwsim.ko
